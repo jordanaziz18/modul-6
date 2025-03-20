eac989b6bbce58ea88a2f853221fe6adfb1c2977
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-03-20 21:22:55 +0700

    (3) Validating request and selectively responding

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("asset/html/hello.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "asset/html/hello.html") // Corrected path
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "asset/html/404.html") // Ensure this file exists too
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
